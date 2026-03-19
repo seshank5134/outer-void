997898bea8d0df9dcb1db2394ab752bcbb804e13
seshank5134 <seshank5134@gmail.com>
2026-03-20 00:49:20 +0530

    fix: CORS policy to allow wildcard origins with Bearer tokens [fix: credentials false]

--- backend/main.py
+++ backend/main.py
@@ -50,7 +50,7 @@ def read_root():
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
-    allow_credentials=True,
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
